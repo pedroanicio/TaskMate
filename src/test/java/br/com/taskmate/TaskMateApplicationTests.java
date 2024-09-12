c6c7d276bc61ff8f01554049570791cc9c5d10aa
pedroanicio <pedroarthurcunha17@gmail.com>
2024-09-11 23:26:42 -0300

    first commit

---
+++ src/test/java/br/com/taskmate/TaskMateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taskmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
